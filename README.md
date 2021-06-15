49a85055a4771962fc3ac745cfa1f07c50594c7c
Emma Stout <emma.stout@ww.com>
2021-06-15 16:17:04 -0500

    add home page and side bar

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ This repo and the accompanying notes come from the [MDN Express/Node tutorial](h
 ### [Model notes](./models/model_notes.md)
 
 ### [Controller notes](./controllers/controller_notes.md)
+
+### Additional notes
+
+#### Async
+- Async is a node module that helps us keep track of multiple asynchronous requests
+- Async is helpful because it allows us to run requests in parallel (or other formations) instead of just chaining all of our requests. Chaining all of our requests may be ineffecient and not the fastest approach. Async allows us to run requests in parallel and then have a single callback that runs when all the requests have completed.
+- `async.series`: Ensures that async operations are performed in series
+- `async.parallel`: Executes async operations in parallel
+  - First argument: collection of async functions to run
+  - `async.parallel` passes each of those functions a `callback(err, result)` which it calls on completion
+  - Optional second argument: callback that will run when all of the async functions have completed
+        - The params of this callback are an error argument and a result collection with the results of all the async functions
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,45 +1,73 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
 
-exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+const async = require('async');
+// Get count of all of the documents, then pass those results to res.render
+exports.index = function (req, res) {
+    async.parallel({
+        book_count: (callback) => {
+            // Here we're using Mongoose: taking our Book model and using the `countDocuments` query on it, 
+            // which (I think) searches our MongoDB collection. So this request is one from the server to the DB, 
+            // as opposed to from the client to the server.
+            Book.countDocuments({}, callback); // Pass an empty object as the match condition to count all books
+        },
+        book_instance_count: function (callback) {
+            BookInstance.countDocuments({}, callback);
+        },
+        book_instance_available_count: function (callback) {
+            BookInstance.countDocuments({ status: 'Available' }, callback);
+        },
+        author_count: function (callback) {
+            Author.countDocuments({}, callback);
+        },
+        genre_count: function (callback) {
+            Genre.countDocuments({}, callback);
+        }
+    }, function (err, results) {
+        // This specifies to pass to the /index views file
+        // The data object will be used by pug to populate the view
+        res.render('index', { title: 'Local Library Home', error: err, data: results })
+    })
 };
 
 // Display list of all books.
-exports.book_list = function(req, res) {
+exports.book_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Book list');
 };
 
 // Display detail page for a specific book.
-exports.book_detail = function(req, res) {
+exports.book_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
 };
 
 // Display book create form on GET.
-exports.book_create_get = function(req, res) {
+exports.book_create_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book create GET');
 };
 
 // Handle book create on POST.
-exports.book_create_post = function(req, res) {
+exports.book_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book create POST');
 };
 
 // Display book delete form on GET.
-exports.book_delete_get = function(req, res) {
+exports.book_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = function(req, res) {
+exports.book_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
+exports.book_update_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book update GET');
 };
 
 // Handle book update on POST.
-exports.book_update_post = function(req, res) {
+exports.book_update_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book update POST');
 };
